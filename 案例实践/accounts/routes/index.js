178370517741cac7aa0e5cb479fe299358f80b9b
giuly.zhang <1659815198@qq.com>
2026-05-13 22:24:38 +0800

    add to list and newly added static resource files

--- 案例实践/accounts/routes/index.js
+++ 案例实践/accounts/routes/index.js
@@ -1,20 +1,30 @@
+/*
+ * Author  Giuly.Zhang
+ * Date  2026-05-09 11:16:46
+ * LastEditors  Giuly.Zhang
+ * LastEditTime  2026-05-13 22:16:00
+ * Description
+ */
 var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   // res.render("index", { title: "Express" });
-  res.send("记账本首页");
+  // res.send("记账本首页");
+  res.render("list");
 });
 
 // 记账本列表
-router.get("/accounts", (req, res) => {
-  res.send("记账本列表");
+router.get("/list", (req, res) => {
+  // res.send("记账本列表");
+  res.render("list");
 });
 
 // 添加记录
-router.get("/accounts/add", (req, res) => {
-  res.send("添加记录");
+router.get("/list/create", (req, res) => {
+  // res.send("添加记录");
+  res.render("create");
 });
 
 module.exports = router;
